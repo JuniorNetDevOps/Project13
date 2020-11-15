89cd646cf80804cf0f2558a8fc91dd9a654947c9
N0016 <rivai@nosairis.com>
2020-11-15 16:32:42 +0800

    Add comments

--- Cisco Systems/rtr2.show_running-config.py
+++ Cisco Systems/rtr2.show_running-config.py
@@ -1,22 +1,21 @@
 from netmiko import ConnectHandler
 
 
-# DEFINE THE ROUTER's DETAILS
-rtr1 = {
+# Define the router's details
+rtr2 = {
     'device_type': 'cisco_ios',
-    'host': '192.168.16.33',
+    'host': '192.168.16.34',
     'port': '22',
     'username': 'kenshin',
     'password': 's3cur3@2020',
 }
 
 
+# Connect to router using SSH
 net_connect = ConnectHandler(**rtr1)
-output = net_connect.send_command('\nshow running-config interface ethernet 0/0')
-output += net_connect.send_command('\nshow ip interface brief')
-output += net_connect.send_command('\nshow ip route 0.0.0.0 0.0.0.0')
+output = net_connect.send_command('show running-config')
 
 
-print(f"\n\n--------- {rtr1['host']} ---------")
+print(f"\n\n--------- {rtr2['host']} ---------")
 print(output)
 print("--------- END ---------")
